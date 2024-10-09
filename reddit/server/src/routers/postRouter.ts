eca24a14c458e8da211de888a1d70b5a4c3be8ec
sanyi <sandor.vass@tuta.io>
2024-10-09 21:02:22 +0200

    feat(): add error handling

--- reddit/server/src/routers/postRouter.ts
+++ reddit/server/src/routers/postRouter.ts
@@ -1,19 +1,50 @@
 import { getAllPosts, getPostById, createPost, votePost } from '../services/postService';
-import { t } from '../trpc';
+import { postOutputSchema, postsArraySchema } from '../schemas/postSchema';
+import { t,  } from '../trpc';
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 
 export const postRouter = t.router({
   getAllPosts: t.procedure.query(async () => {
-    return await getAllPosts();
+		const posts = await getAllPosts();
+		const validatedPosts = postsArraySchema.safeParse(posts);
+
+    if (!validatedPosts.success) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `We couldn't find any posts`,
+      });
+		}
   }),
 	getPostById: t.procedure.input(z.object({ id: z.bigint() })).query(async ({ input }) => {
-		return await getPostById(input.id);
+		const post = await getPostById(input.id);
+		const validatedPost = postOutputSchema.safeParse(post);
+	  if (!validatedPost.success) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `We couldn't find this post`,
+      });
+		}	
   }),
 	createPost: t.procedure.input(z.object({ title: z.string(), url: z.string()})).mutation(async ({ input }) => {
-		return await createPost(input);
+		const newPost = await createPost(input);
+		const validatedPost = postOutputSchema.safeParse(newPost);
+	  if (!validatedPost.success) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `We couldn't create this post`,
+      });
+		}
   }),
 	votePost: t.procedure.input(z.object({id: z.string().transform((id) => BigInt(id)), vote: z.enum(['up', 'down'])})).mutation(async ({ input }) => {
-		return await votePost(input.id, input.vote);
+		const updatedPost = await votePost(input.id, input.vote);
+		const validatedPost = postOutputSchema.safeParse(updatedPost);
+	  if (!validatedPost.success) {
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: `Something went wrong!`,
+      });
+		}
 	})
 })
 
---
+++ reddit/server/src/schemas/postSchema.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+
+export const postOutputSchema = z.object({
+  id: z.bigint(),
+  title: z.string(),
+  url: z.string().url(),
+  voteCount: z.number(),
+});
+
+export const postsArraySchema = z.array(postOutputSchema);
